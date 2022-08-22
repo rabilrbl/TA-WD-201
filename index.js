ce5823d826eace0c24651882e4ed6f0749bfc4dd
Mohammed Rabil <rabil@techie.com>
2022-08-22 22:28:39 +0530

    Add cli input and navigate with http routes

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-function hello() {
-    console.log("Hello Github! Trying it for the first time");
+const fs = require("fs");
+const http = require("http");
+const readline = require("readline");
+
+const readFile = (fileName) => {
+  try {
+    return fs.readFileSync(fileName);
+  } catch (e) {
+    throw e;
   }
-  
-  hello();
+};
+
+const server = (regPath) => http.createServer(function (request, response) {
+  let url = request.url;
+  response.writeHeader(200, { "Content-Type": "text/html" });
+  switch (url) {
+    case "/project":
+      response.write(readFile("projects.html"));
+      response.end();
+      break;
+    case "/registration":
+      response.write(readFile(regPath));
+      response.end();
+      break;
+    default:
+      response.write(readFile("home.html"));
+      response.end();
+      break;
+  }
+});
+
+const lineDetail = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+lineDetail.question(`Please provide registration file path - `, (path) => {
+  server(path).listen(3000);
+});
