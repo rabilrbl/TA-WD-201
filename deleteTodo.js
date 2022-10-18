848e9ce407ccc7baf42a89630bb8879660f1b05e
Mohammed Rabil <rabil@techie.com>
2022-10-18 22:29:39 +0530

    Feature: Todo list basic operations
    - Add Todo
    - Mark todo as complete
    - delete a todo
    - list all todos

---
+++ deleteTodo.js
@@ -0,0 +1,24 @@
+const argv = require("minimist")(process.argv.slice(2));
+const db = require("./models/index");
+
+const deleteTodo = async (id) => {
+  try {
+    await db.Todo.deleteTask(id);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  const { id } = argv;
+  if (!id) {
+    throw new Error(
+      "id is required. \nSample command: node deleteTodo.js --id=2 "
+    );
+  }
+  if (!Number.isInteger(id)) {
+    throw new Error("The id needs to be an integer");
+  }
+  await deleteTodo(id);
+  await db.Todo.showList();
+})();
