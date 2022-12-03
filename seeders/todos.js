d771675765eddd2ed3b7b627d464d4e323cad2cd
Mohammed Rabil <rabil@techie.com>
2022-12-03 09:07:47 +0530

    Refactor  app

--- seeders/todos.js
+++ seeders/todos.js
@@ -1,11 +1,26 @@
 const { Todo } = require("../models");
 
+const TODOS = [
+  {
+    title: "Buy milk",
+    dueDate: new Date("2020-01-01"),
+    completed: false,
+  },
+  {
+    title: "Buy eggs",
+    dueDate: new Date(),
+    completed: false,
+  },
+  {
+    title: "Buy bread",
+    // TOmorrow
+    dueDate: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+    completed: false,
+  },
+];
+
 module.exports = {
   seedTodo: async () => {
-    await Todo.create({
-      title: "Learn Express",
-      dueDate: "2020-10-10",
-      completed: true,
-    });
+    await Todo.bulkCreate(TODOS);
   },
 };
