f93cca19d26292cfd99a0cedcd25d9cfde768512
Mohammed Rabil <rabil@techie.com>
2022-12-24 20:20:40 +0530

    Add extra constraints for email attrivute

--- models/users.js
+++ models/users.js
@@ -15,7 +15,14 @@ module.exports = (sequelize, DataTypes) => {
     {
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
       password: DataTypes.STRING,
     },
     {
